8e898343bf1a49d4c2ad4979c1d21f172ce37bcb
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-02-10 06:28:40 +0200

    refactor entry file

--- CSharp-Notes/Program.cs
+++ CSharp-Notes/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharpNotes
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("C Sharp Reference Notes");
+		}
+	}
+}
 
